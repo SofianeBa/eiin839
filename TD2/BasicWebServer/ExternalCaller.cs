045527618de37630078db1a7fc02089dafe21036
Sofiane. B <sofianebabouri9@gmail.com>
2021-02-21 08:38:25 +0100

    TD 2 - Quest 4 et 5 (essai)

--- TD2/BasicWebServer/ExternalCaller.cs
+++ TD2/BasicWebServer/ExternalCaller.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 
-class Program
+class ExternalCaller
 {
-    static void Main()
+    public static string Main(HttpListenerRequest request)
     {
         //
         // Set up the process with the ProcessStartInfo class.
@@ -12,8 +13,8 @@ class Program
         //
         ProcessStartInfo start = new ProcessStartInfo();
         start.FileName = @"C:\Users\SofianeB\Desktop\Polytech\4A\Soc\TD2\ExecTest\bin\Debug\ExecTest.exe"; // Specify exe name.
-        start.Arguments = "Argument1"; // Specify arguments.
-        start.UseShellExecute = false; 
+        start.Arguments = request.Url.Query; // Specify arguments.
+        start.UseShellExecute = false;
         start.RedirectStandardOutput = true;
         //
         // Start the process.
@@ -25,9 +26,7 @@ class Program
             //
             using (StreamReader reader = process.StandardOutput)
             {
-                string result = reader.ReadToEnd();
-                Console.WriteLine(result);
-                Console.ReadLine();
+                return reader.ReadToEnd();
             }
         }
     }
---
+++ TD2/MyMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+
+namespace MyMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+                Console.WriteLine("<HTML><BODY> Hello " + HttpUtility.ParseQueryString(args[0]).Get("param1") + " " + HttpUtility.ParseQueryString(args[0]).Get("param2") + "</BODY></HTML>");
+            else
+                Console.WriteLine("ExeTest <string parameter>");
+        }
+    }
+}
